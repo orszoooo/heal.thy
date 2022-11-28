e4dae4b7e0e12f6c6187be8b777c42a22c9ca96c
orszoooo <117857476+orszoooo@users.noreply.github.com>
2022-11-28 20:41:57 +0100

    Created basic templates and simple view.

--- base/views.py
+++ base/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def homePage(request):
-    return render(request, 'home-page')
+    context = {}
+    return render(request, 'base/home.html', context)
